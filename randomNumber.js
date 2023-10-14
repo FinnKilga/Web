1664e03f0dcf76f2e644f6a68fa6bae8ea55a0e1
FinnKilga <finn.kilga@student.htldornbirn.at>
2023-10-14 15:19:06 +0200

    skipped js, did these kind of excercises with cpp

---
+++ randomNumber.js
@@ -0,0 +1,40 @@
+//06+08
+
+let randomNumber = Math.random() * 5 + 5;
+
+let random = Math.floor(randomNumber);
+
+switch (random) {
+    case 10:
+        console.log("ten");
+        break;
+    case 9:
+        console.log("nine");
+        break;
+    case 8:
+        console.log("eight");
+        break;
+    case 7:
+        console.log("seven");
+        break;
+    case 6:
+        console.log("six");
+        break;
+    case 5:
+        console.log("five");
+        break;
+    case 4:
+        console.log("four");
+        break;
+    case 3:
+        console.log("three");
+        break;
+    case 2:
+        console.log("two");
+        break;
+    case 1:
+        console.log("one");
+        break;
+    default:
+        console.log("null");
+}
