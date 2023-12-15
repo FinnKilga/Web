97f25315b510411dd557493ab047d875d595ce1f
FinnKilga <finn.kilga@student.htldornbirn.at>
2023-12-15 07:23:15 +0100

    button remembers it's state and changes color when clicked

--- react/react_test/src/components/Button.js
+++ react/react_test/src/components/Button.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Button.module.css'
 
 export default function Button(probs){
+    const[isOn,setIsOn] = useState(false);
     return(
-        <div className={styles.main} onClick={()=>{
-            alert("kick von Hannes in die Fresse")
-        }}>{probs.name} is {probs.age} years old </div>
+        <div className={isOn?styles.isOn:styles.isOff} onClick={()=>{
+            //alert("kick von Hannes in die Fresse")
+            setIsOn(!isOn);
+        }}>{probs.name} is {probs.age} years old and button is {isOn?"On":"Of"}</div>
     )
 }
